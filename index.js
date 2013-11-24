39473184895132f9899a4f07ac5863b9b21dd3ec
Hector Correa <hector@hectorcorrea.com>
2013-11-24 12:52:16 -0500

    Added link to github repo

--- index.js
+++ index.js
@@ -2,32 +2,8 @@
 // handles keeping the connection open and
 // dealing with reconnections.
 //
-// In Node.js applications MongoDB connections 
-// are meant to be opened once and kept opened 
-// while the Node.js app is running 
-// (http://stackoverflow.com/a/15688610/446681).
-// This code encapsulates the logic required to
-// do this.  
+// More info: https://github.com/hectorcorrea/mongoConnect
 //
-// Be aware that currently only one database can 
-// be handled through this code. As such, only the 
-// first call to setup is honored, subsequent calls 
-// are ignored.
-//
-// Typical usage is:
-//  
-//  // In your main server.js
-//  var mongoConnect = require('./mongoConnect');
-//  mongoConnect.setup('mongodb://localhost:27017/yourDB');
-//
-//  // In your database access code
-//  var mongoConnect = require('./mongoConnect');
-//  mongoConnect.execute(function(err, db) {
-//    Your code to do something with the db goes here
-//    e.g. db.collection('xxx').find(...)
-//  })
-//
-
 var MongoClient = require('mongodb').MongoClient;
 var dbUrl = null; 
 var isConnected = false;
